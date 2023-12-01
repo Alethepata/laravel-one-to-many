702fd070681dca86d61a13efd1c45ae2e57a24fd
Alethepata <alessia.patane@gmail.com>
2023-12-01 17:06:51 +0100

    [Add] type

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -11,6 +11,10 @@
     <h1>Progetto : {{$project->title}}</h1>
     <p>Progetto iniziato il : {{$project->start_date }} | Progetto finito il : {{$project->end_date }}</p>
     <p>Descrizione : {{$project->explanation}}</p>
+
+    @if ($project->type)
+        <p>Tipo : {{$project->type->name}}</p>
+    @endif
     <a class="btn btn-outline-warning" href="{{route('admin.projects.edit', $project )}}"><i class="fa-solid fa-pen-to-square"></i></a>
 
    @include('admin.partials.delete', [
