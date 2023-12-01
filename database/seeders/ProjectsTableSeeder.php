82874a6a63a91a79a6c5e25033c5400725a2139b
Alethepata <alessia.patane@gmail.com>
2023-12-01 16:51:47 +0100

    [Add] $new_project->type_id

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 use App\Models\Project;
+use App\Models\Type;
 use Faker\Generator as Faker;
 
 class ProjectsTableSeeder extends Seeder
@@ -19,6 +20,7 @@ class ProjectsTableSeeder extends Seeder
     {
         for($i = 0; $i < 10; $i++){
             $new_project = new Project();
+            $new_project->type_id = Type::inRandomOrder()->first()->id;
             $new_project->title = $faker->sentence();
             $new_project->slug = Project::generateSlug($new_project->title);
             $new_project->start_date = $faker->date();
